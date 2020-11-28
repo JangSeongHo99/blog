b2b2c8a6c1250b274aef8bd4194bd1e026f59931
JangSeongHo99 <qq9725@ajou.ac.kr>
2020-11-28 07:58:56 +0000

    list of IT development career

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+	
+	let [글제목,글제목변경] = useState(['제5회 대한민국 SW융합 해커톤 대회', '빅데이터 기반 국민참여형 대중교통 문제 개선방안 공모전', '2020 군장병 SW 해커톤 대회']);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    	<div className="black-nav">
+			<div>개발 Blog</div>
+		</div>
+    	<div className="list">
+			<h3> {글제목[0]}</h3>
+			<p>2018.8.30 ~ 2018.9.1</p>
+			<hr/>
+		</div>
+		<div className="list">
+			<h3> {글제목[1]}</h3>
+			<p>2018.12.15</p>
+			<hr/>
+		</div>
+		<div className="list">
+			<h3> {글제목[2]}</h3>
+			<p>2020.09.16 ~ 2020.11.12</p>
+			<hr/>
+		</div>
     </div>
   );
 }
